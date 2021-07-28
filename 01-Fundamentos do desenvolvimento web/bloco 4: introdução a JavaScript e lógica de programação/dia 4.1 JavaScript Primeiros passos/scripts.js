0de2ac384d482d4cf1e1c98e5ad0088ec5ce4194
Monte <you@example.com>
2021-07-28 17:36:40 -0300

    .

---
+++ 01-Fundamentos do desenvolvimento web/bloco 4: introdução a JavaScript e lógica de programação/dia 4.1 JavaScript Primeiros passos/scripts.js
@@ -0,0 +1,11 @@
+let notaDesafio = 70;
+
+if (notaDesafio >= 80){
+  console.log("Parabéns, você foi aprovado!")
+}
+else if (notaDesafio < 80 >= 60){
+console.log("Você está na nossa lista de espera")
+}
+else {
+  console.log("Você foi reprovado")
+}
