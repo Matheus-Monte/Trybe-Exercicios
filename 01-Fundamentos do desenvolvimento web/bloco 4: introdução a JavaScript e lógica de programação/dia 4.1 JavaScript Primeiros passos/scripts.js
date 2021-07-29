b60b55dee043eec618bc8bd0114022ebca98c09e
Monte <you@example.com>
2021-07-29 18:10:50 -0300

    exercicio 1

--- 01-Fundamentos do desenvolvimento web/bloco 4: introdução a JavaScript e lógica de programação/dia 4.1 JavaScript Primeiros passos/scripts.js
+++ 01-Fundamentos do desenvolvimento web/bloco 4: introdução a JavaScript e lógica de programação/dia 4.1 JavaScript Primeiros passos/scripts.js
@@ -1,23 +1,5 @@
-let nota = 98;
+let number1 = 8;
+let number2 = 9;
+let number3 = 3;
 
-if (nota < 0 || nota > 100){
-  console.log('ERROR')
-  }
-  else if(nota >= 90) {
-    console.log('A');
-  }
-  else if(nota >= 80) {
-    console.log('B');
-  }
-  else if(nota >= 70) {
-    console.log('C');
-  }
-  else if(nota >= 60) {
-    console.log('D');
-  }
-  else if(nota >= 50) {
-    console.log('E');
-  }
-  else {
-    console.log('F')
-  }
+if()
